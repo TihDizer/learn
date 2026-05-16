83fd53626ed4bce2c9297fe61a24002d09b38faf
tihdizer <tihdizer@gmail.com>
2026-05-17 00:59:23 +0300

    feat: remove venv and add playwright

--- lab5/flake.nix
+++ lab5/flake.nix
@@ -11,22 +11,14 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           python313
+          python313Packages.playwright
           stdenv.cc.cc.lib
           chromium
           chromedriver
         ];
-        shellHook = ''
-          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:\$LD_LIBRARY_PATH"
 
-          if [ ! -d ".venv" ]; then
-            python3 -m venv .venv
-          fi
-          source .venv/bin/activate
-          pip install --upgrade pip > /dev/null
-          if [ -f "requirements.txt" ]; then
-            pip install -r requirements.txt > /dev/null
-          fi
-          echo "Nix dev shell activated (venv ready)"
+        shellHook = ''
+          echo "Nix dev shell activated"
           trap 'deactivate > /dev/null; echo "Nix dev shell deactivated"' EXIT
         '';
       };
