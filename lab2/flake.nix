daa276ae95cee633ef61cdd2209657aa91ce1939
tihdizer <tihdizer@gmail.com>
2026-05-21 16:00:42 +0300

    feat: add insomnia for task 5

--- lab2/flake.nix
+++ lab2/flake.nix
@@ -18,20 +18,31 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           postman
+          insomnia
         ];
 
         shellHook = ''
           echo "Nix dev shell activated"
 
           mkdir -p .logs
+
           postman > .logs/postman.log 2>&1 &
           POSTMAN_PID=$!
+
+          insomnia > .logs/insomnia.log 2>&1 &
+          INSOMNIA_PID=$!
 
           cleanup() {
             if kill -0 "$POSTMAN_PID" 2>/dev/null; then
               kill "$POSTMAN_PID" 2>/dev/null || true
               wait "$POSTMAN_PID" 2>/dev/null || true
             fi
+
+            if kill -0 "$INSOMNIA_PID" 2>/dev/null; then
+              kill "$INSOMNIA_PID" 2>/dev/null || true
+              wait "$INSOMNIA_PID" 2>/dev/null || true
+            fi
+
             echo "Nix dev shell deactivated"
           }
 
