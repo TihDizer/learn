34e0725d8db6097f86781f3ba2aab83b1b0bd4ae
tihdizer <tihdizer@gmail.com>
2026-05-27 11:43:13 +0300

    feat: add 2nd instance

--- lab2/main.tf
+++ lab2/main.tf
@@ -6,92 +6,65 @@ terraform {
   }
   required_version = ">= 0.13"
 }
-
 provider "yandex" {
-  zone      = "ru-central1-a"
-  token     = var.token
-  cloud_id  = var.cloud_id
+  zone     = "ru-central1-a"
+  token    = var.token
+  cloud_id = var.cloud_id
   folder_id = var.folder_id
 }
 
 variable "token" { type = string }
 variable "cloud_id" { type = string }
 variable "folder_id" { type = string }
-variable "ssh_key_path" {
-  type    = string
-  default = "~/.ssh/id_rsa.pub"
-}
 
 data "yandex_compute_image" "ubuntu" {
   family = "ubuntu-2204-lts"
 }
 
-# Network
-resource "yandex_vpc_network" "lab-net" {
-  name = "lab-network"
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-ssd"
+  zone     = "ru-central1-a"
+  size     = "20"
+  image_id = data.yandex_compute_image.ubuntu.id
 }
 
-resource "yandex_vpc_subnet" "lab-subnet" {
-  name           = "lab-subnet"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.lab-net.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
+resource "yandex_compute_disk" "boot-disk-2" {
+  name     = "boot-disk-2"
+  type     = "network-ssd"
+  zone     = "ru-central1-a"
+  size     = "20"
+  image_id = data.yandex_compute_image.ubuntu.id
 }
 
-# Security Group
-resource "yandex_vpc_security_group" "lab-sg" {
-  name       = "lab-sg"
-  network_id = yandex_vpc_network.lab-net.id
+resource "yandex_compute_instance" "lab-gitlab" {
+  name = "lab-gitlab"
 
-  ingress {
-    protocol       = "TCP"
-    port           = 80
-    v4_cidr_blocks = ["0.0.0.0/0"]
+  labels = {
+   environment = "network-1"
+   owner       = "tihdizer"
+   task        = "block-2"
   }
-
-  ingress {
-    protocol       = "TCP"
-    port           = 443
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol       = "TCP"
-    port           = 22
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# GitLab Instance
-resource "yandex_compute_instance" "gitlab" {
-  name = "gitlab-server"
-  zone = "ru-central1-a"
 
   resources {
     cores  = 4
     memory = 8
   }
 
+  allow_stopping_for_update = true
+
   boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.id
-      size     = 30
-    }
+    disk_id = yandex_compute_disk.boot-disk-1.id
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.lab-subnet.id
+    subnet_id = yandex_vpc_subnet.subnet-1.id
     nat       = true
-    security_group_ids = [yandex_vpc_security_group.lab-sg.id]
+    security_group_ids = [yandex_vpc_security_group.web_sg.id]
   }
 
   metadata = {
-    ssh-keys  = "ubuntu:${file(var.ssh_key_path)}"
+    ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
     user-data = <<-EOF
       #cloud-config
       package_update: true
@@ -101,73 +74,120 @@ resource "yandex_compute_instance" "gitlab" {
       runcmd:
         - systemctl enable docker
         - systemctl start docker
+        - usermod -aG docker ubuntu
+
         - mkdir -p /home/ubuntu/gitlab
+
         - |
           cat > /home/ubuntu/gitlab/docker-compose.yml <<EOL
           version: '3.6'
           services:
-            gitlab:
-              image: gitlab/gitlab-ce:latest
-              container_name: gitlab
-              restart: unless-stopped
-              hostname: 'gitlab.local'
-              ports:
-                - "80:80"
-                - "443:443"
-                - "2222:22"
-              volumes:
-                - ./config:/etc/gitlab
-                - ./logs:/var/log/gitlab
-                - ./data:/var/opt/gitlab
+           gitlab:
+            image: gitlab/gitlab-ce:latest
+            container_name: gitlab
+            restart: unless-stopped
+            hostname: 'gitlab.local'
+            ports:
+              - "80:80"
+              - "443:443"
+              - "2222:22"
+            volumes:
+              - ./gitlab/config:/etc/gitlab
+              - ./gitlab/logs:/var/log/gitlab
+              - ./gitlab/data:/var/opt/gitlab
+            environment:
+              GITLAB_OMNIBUS_CONFIG: |
+                nginx['listen_addresses'] = ['0.0.0.0']
           EOL
+
         - cd /home/ubuntu/gitlab && docker-compose up -d
     EOF
   }
+
+  lifecycle {
+    create_before_destroy = true
+
+    prevent_destroy = false
+
+    ignore_changes = [labels, metadata]
+  }
 }
 
-# GitLab Runner Instance
-resource "yandex_compute_instance" "runner" {
-  name = "gitlab-runner"
-  zone = "ru-central1-a"
+resource "yandex_compute_instance" "deploy" {
+  name = "deploy"
 
-  resources {
-    cores  = 2
-    memory = 4
+  labels = {
+   environment = "network-1"
+   owner       = "tihdizer"
+   task        = "block-2"
   }
 
+  resources {
+    cores  = 4
+    memory = 8
+  }
+
+  allow_stopping_for_update = true
+
   boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.id
-      size     = 20
-    }
+    disk_id = yandex_compute_disk.boot-disk-2.id
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.lab-subnet.id
+    subnet_id = yandex_vpc_subnet.subnet-1.id
     nat       = true
-    security_group_ids = [yandex_vpc_security_group.lab-sg.id]
+    security_group_ids = [yandex_vpc_security_group.web_sg.id]
   }
 
   metadata = {
-    ssh-keys  = "ubuntu:${file(var.ssh_key_path)}"
-    user-data = <<-EOF
-      #cloud-config
-      package_update: true
-      packages:
-        - docker.io
-      runcmd:
-        - systemctl enable docker
-        - systemctl start docker
-        - curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
-        - apt-get install -y gitlab-runner
-    EOF
+    ssh-keys  = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+
+    prevent_destroy = false
+
+    ignore_changes = [labels, metadata]
   }
 }
 
-output "gitlab_public_ip" {
-  value = yandex_compute_instance.gitlab.network_interface.0.nat_ip_address
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
 }
 
-output "runner_public_ip" {
-  value = yandex_compute_instance.runner.network_interface.0.nat_ip_address
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["10.0.1.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.lab-gitlab.network_interface.0.nat_ip_address
+}
+
+output "ssh_command" {
+  value = "ssh ubuntu@${yandex_compute_instance.lab-gitlab.network_interface.0.nat_ip_address}"
+}
+
+resource "yandex_vpc_security_group" "web_sg" {
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    protocol       = "TCP"
+    port           = 80
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      protocol       = "TCP"
+      port           = 22
+      v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
 }
