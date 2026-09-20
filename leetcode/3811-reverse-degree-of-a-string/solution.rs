56b849d1296fb083b397dfcca8d6f1a3cd44e624
Ivan Lapshin <tihdizer@gmail.com>
2026-09-20 13:28:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 2.2 MB (12.43%)

--- leetcode/3811-reverse-degree-of-a-string/solution.rs
+++ leetcode/3811-reverse-degree-of-a-string/solution.rs
@@ -1,10 +1,8 @@
 impl Solution {
     pub fn reverse_degree(s: String) -> i32 {
-        let mut ans = 0;
-        for (i, ch) in s.as_bytes().into_iter().enumerate() {
-            let r_ch = b'z' + 1 - ch;
-            ans += r_ch as i32 * (i as i32 + 1);
-        }
-        ans
+        s.bytes()
+            .enumerate()
+            .map(|(i, ch)| (b'z' + 1 - ch) as i32 * (i as i32 + 1))
+            .sum()
     }
 }
