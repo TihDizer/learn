2e43df4952bab8df628580af5cfa246d7b40554d
Ivan Lapshin <tihdizer@gmail.com>
2026-09-23 12:22:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 4 MB (9.68%)

--- leetcode/2634-minimum-common-value/solution.rs
+++ leetcode/2634-minimum-common-value/solution.rs
@@ -1,16 +1,17 @@
+use std::cmp::Ordering;
+
 impl Solution {
     pub fn get_common(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-        let mut i = 0;
-        let mut j = 0;
+        let (mut i, mut j) = (0, 0);
+
         while i < nums1.len() && j < nums2.len() {
-            if nums1[i] == nums2[j] {
-                return nums1[i];
-            } else if nums1[i] > nums2[j] {
-                j += 1;
-            } else if nums2[j] > nums1[i] {
-                i += 1;
+            match nums1[i].cmp(&nums2[j]) {
+                Ordering::Equal => return nums1[i],
+                Ordering::Less => i += 1,
+                Ordering::Greater => j += 1,
             }
         }
+
         -1
     }
 }
